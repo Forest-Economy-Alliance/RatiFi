1314da0dfe262c7b2ccccb89c18532fd5ba544f9
Siddharth Kumar Yadav <siddharthsk1234@gmail.com>
2022-06-13 18:47:07 +0530

    added background Image

--- components/Background.js
+++ components/Background.js
@@ -1,14 +1,17 @@
 import React from 'react';
-import {View} from 'react-native';
+import {View, ImageBackground} from 'react-native';
 
 export default function Background(props) {
+  const BG_IMG_PATH = require('../assets/images/background.png');
   return (
     <View
       style={{
         flex: 1,
       }}
     >
-      {props.children}
+      <ImageBackground blurRadius={5} source={BG_IMG_PATH} resizeMode="cover">
+        {props.children}
+      </ImageBackground>
     </View>
   );
 }
